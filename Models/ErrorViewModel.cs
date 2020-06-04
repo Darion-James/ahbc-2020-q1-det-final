5aad113b8903828dd473b06b1110bc5f548fccbf
Jakyle <jjackson360@gmail.com>
2020-06-04 18:58:24 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ahbc2020final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
